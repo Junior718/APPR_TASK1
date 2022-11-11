32817b26fe5995ff22f759fa8a7c62ad82d9f26a
Mulalo Mulaudzi <ST10118049@vcconnect.edu.za>
2022-11-11 13:30:00 +0200

    i crerated a report and active disaster controller , the migrated the database, and updated the database there after.

---
+++ Migrations/20221111112422_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APPR_TASK1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActiveDisaster",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisasterType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActiveDisaster", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    totalMonetaryDonationsReceived = table.Column<double>(nullable: false),
+                    totalNumberOfGoodsReceived = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActiveDisaster");
+
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
